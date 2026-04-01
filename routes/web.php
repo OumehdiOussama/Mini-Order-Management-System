c92c1c77583d395ef9261e3ef42b402879b20de7
Oussama Oumehdi <oumehdioussama@gmail.com>
2026-04-01 11:01:24 +0100

    Add resource routes for Customer, Product, and Order controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('customers', CustomerController::class);
+Route::resource('products', ProductController::class);
+Route::resource('orders', OrderController::class);
